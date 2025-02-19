27e4a8bf03e61e09dddcf43002a073b861ff0b79
Toky Ralala <ralala.toky@gmail.com>
2025-02-19 10:43:23 -0500

    pm2 adjustment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,20 @@ module.exports = {
   apps: [
     {
       name: 'eliza-agent',
-      cwd: '/var/www/eliza/agent',
+      cwd: '/var/www/eliza',
       script: 'pnpm',
       args: 'start',
       env: {
         NODE_ENV: 'production',
         SERVER_PORT: 3000
-      }
+      },
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '2G',
     },
     {
       name: 'eliza-client',
-      cwd: '/var/www/eliza/client',
+      cwd: '/var/www/eliza',
       script: 'pnpm',
       args: 'dev --host',
       env: {
